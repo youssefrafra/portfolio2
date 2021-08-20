37b228f8358df53ea8514032434d45eb7481002f
youssef rafrafi <rafrafi.youssef@hotmail.com>
2021-08-20 17:26:35 +0200

    correction ortho

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ return (
   <Section id="about">
     <SectionTitle>À propos de moi</SectionTitle>
     <SectionText>
-      Après une Licence en Maths et étant passioné par le métier j'ai décidé de me reconvertir 
+      Après une Licence en Maths et étant passionné par le métier j'ai décidé de me reconvertir 
       dans le métier de développeur web. Autonome de nature, je peux travailler seul ou en groupe, 
       capable de m’adapter à un environnement que je ne connais pas en cherchant toujours à améliorer et optimiser 
       les projets au maximum pour satisfaire au mieux la demande du client.
