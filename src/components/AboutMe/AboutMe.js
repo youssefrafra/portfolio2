d4c5ef0c58297e77726ad3d8c3d9ec6cf1af5e14
youssef rafrafi <rafrafi.youssef@hotmail.com>
2021-11-09 19:23:53 +0100

    Edited about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,8 +11,8 @@ return (
   <Section id="about">
     <SectionTitle>À propos de moi</SectionTitle>
     <SectionText>
-      Après une Licence en Maths et étant passionné par le métier j'ai décidé de me reconvertir 
-      dans le métier de la programmation et la science des données . Autonome de nature, je peux travailler seul ou en groupe, 
+      Après une Licence en Maths et étant passionné par la programmation et la science des données j'ai décidé de me reconvertir 
+      dans le monde de l'intelligence artificielle et le deep learning . Autonome de nature, je peux travailler seul ou en groupe, 
       capable de m’adapter à un environnement que je ne connais pas en cherchant toujours à améliorer et optimiser 
       les projets au maximum pour satisfaire au mieux la demande du client.
     </SectionText>
